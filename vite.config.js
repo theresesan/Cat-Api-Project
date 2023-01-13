0eedf18a96c73390d0d880a739c6cfe7aa16787a
Therese Hagen <therese.kodehode@gmail.com>
2023-01-13 09:50:22 +0100

    new

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Cat-Api-Project/",
   plugins: [react()],
+  base: "/Cat-Api-Project/",
 });
